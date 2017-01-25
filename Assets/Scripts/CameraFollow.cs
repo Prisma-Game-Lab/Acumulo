50e8db1bbe50c252ff370311251e521f86d2739b
Rudá Fraga Valle <rudavalle@gmail.com>
2017-01-25 16:55:21 -0200

    Camera follow and camera clamp

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+    public GameObject Player;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 v3 = new Vector3(Player.transform.position.x,Player.transform.position.y,transform.position.z);
+        transform.position = v3;
+	}
+}
---
+++ Assets/Scripts/Camera_Bounds.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera_Bounds : MonoBehaviour {
+
+	//mapX, mapY is size of background image
+         private float mapX = 63.7f; 
+         private float mapY = 62.6f;
+         private float minX;
+         private float maxX;
+         private float minY;
+         private float maxY;
+     
+         void Update()
+            {
+            float vertExtent = GetComponent<Camera>().orthographicSize;  
+            float  horzExtent = vertExtent * Screen.width / Screen.height;
+             
+         // Calculations assume map is position at the origin
+             minX = horzExtent - mapX / 2.0f;
+             maxX = mapX / 2.0f - horzExtent ;
+             minY = vertExtent - mapY / 2.0f;
+             maxY = mapY / 2.0f - vertExtent;
+         }
+     
+         void LateUpdate() 
+         {
+             Vector3 v3 = transform.position;
+             v3.x = Mathf.Clamp(v3.x, minX, maxX);
+             v3.y = Mathf.Clamp(v3.y, minY, maxY);
+             transform.position = v3;
+         }
+}
