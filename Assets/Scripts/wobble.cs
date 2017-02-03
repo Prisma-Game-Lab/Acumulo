2a9383370f61b0bda218e56ca2ded4f8a7e1e4aa
Leonardo Kaplan <leokaplan@users.noreply.github.com>
2017-02-03 16:55:49 -0200

    wobble effect
    valores razoaveis: 1 e 1

---
+++ Assets/Scripts/wobble.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class wobble : MonoBehaviour {
+
+    
+    public float speed, intensity;
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.forward, Mathf.Sin(speed * Time.time) * intensity * Random.value );
+    }
+}
