44d281b72ef21bebf0ec7af7ad1ea69b688f7815
Felipe Pessoa <felipessoaf@hotmail.com>
2017-02-11 18:18:37 -0200

    Animatic test.

---
+++ Assets/Scripts/Animatic.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Animatic : MonoBehaviour
+{
+    private MovieTexture _animatic;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Renderer r = GetComponent<Renderer>();
+        _animatic = (MovieTexture)r.material.mainTexture;
+        _animatic.Play();
+    }
+
+    private void Update()
+    {
+        if(!_animatic.isPlaying || Input.anyKeyDown)
+        {
+            SceneManager.LoadScene("DevScene");
+        }
+    }
+}
